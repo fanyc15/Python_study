7eb629cf692c5316a126b9f94387619a8b7065f7
fanyc <729247164@qq.com>
2022-07-23 22:35:13 +0800

    commit 4

--- 学习/027函数返回值.py
+++ 学习/027函数返回值.py
@@ -13,6 +13,9 @@ def sum(num1, num2):
         raise TypeError('参数类型错误')
     return num1 + num2
 
+print(sum(1, 2))
+# 还通过内置函数isinstance()进行数据类型检查，检查调用函数时参数是否是整形和浮点型。如果参数类型不对，会报错，提示 参数类型错误
+# print(sum("11", 2))
 
 # 当然，函数也可以返回多个值，具体实例如下：
 def division(num1, num2):
@@ -28,6 +31,3 @@ tuple1 = division(9, 4)
 print(num1, num2)
 print(tuple1)
 
-print(sum(1, 2))
-# 还通过内置函数isinstance()进行数据类型检查，检查调用函数时参数是否是整形和浮点型。如果参数类型不对，会报错，提示 参数类型错误
-# print(sum("11", 2))
