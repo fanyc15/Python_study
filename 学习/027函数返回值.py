2d31961b88475a7a45f3e2e2587ef1422dd92511
fanyc <729247164@qq.com>
2022-07-23 22:28:35 +0800

    commit 3

--- 学习/027函数返回值.py
+++ 学习/027函数返回值.py
@@ -14,6 +14,20 @@ def sum(num1, num2):
     return num1 + num2
 
 
+# 当然，函数也可以返回多个值，具体实例如下：
+def division(num1, num2):
+    # 求商与余数
+    a = num1 % num2
+    b = (num1 - a) / num2
+    return b, a
+
+
+num1, num2 = division(9, 4)
+tuple1 = division(9, 4)
+
+print(num1, num2)
+print(tuple1)
+
 print(sum(1, 2))
 # 还通过内置函数isinstance()进行数据类型检查，检查调用函数时参数是否是整形和浮点型。如果参数类型不对，会报错，提示 参数类型错误
 # print(sum("11", 2))
