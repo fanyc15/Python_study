62085341946f5188b6d11db0ebf6603c073f066c
fanyc <729247164@qq.com>
2022-07-27 23:09:11 +0800

    commit 2

---
+++ pycharm使用技巧/001.py
@@ -0,0 +1,52 @@
+# 1. .print写出print()
+a1 = 15
+b = 20
+# a+b .print
+print(a1 + b)
+
+
+# 2. shift F6快捷修改
+
+def function():
+    ...
+
+
+# 3.ctrl + alt +L   快速格式
+
+# 4. alt + shift + ↑、↓  快速对调位置
+
+# 5. 按住alt键  多个位置添加
+# name = data["123userName"]
+# job = data["123jobName"]
+
+# 6. 按住鼠标中键  多个位置添加
+# name123 = data["123userName"]
+# name123 = data["123userName"]
+# name123 = data["123userName"]
+# name123 = data["123userName"]
+
+# 7 .ctrl + tab 文件切换器
+
+# 8 .alt + tab 窗口切换器
+
+# 9.shift选择代码 ctrl / 注释代码
+
+# 10.扩散 ctrl + w
+
+# 11.ctrl + f 单个文件搜索
+
+# 12 . todo Todo  标注位置
+
+def say(self):
+    # todo say函数
+    print(f"大家好，我是大学生，我的名字叫{self.name}")
+# 13. ctrl + shift 数字键设置书签
+
+# 14.打开文件  右键打开于explorer
+
+# 15 ctrl + shift F10  运行
+
+# 16.   右下角 视图模式 浮动
+
+# 编辑器模板
+# 文件设置》》编辑器》》文件与代码模板》》python scipt 》》设置你的通用模板
